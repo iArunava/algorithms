a5ca1bc0cafb52b2746c73d478cfc76205057680
Arunava <learningdroidarunava@gmail.com>
2025-02-07 10:14:24 +0530

    Updates

---
+++ 706/706.java
@@ -0,0 +1,14 @@
+class MyHashMap {
+
+    public MyHashMap() {
+        lists = new List[ksize];
+        for (int i = 0; i < ksize; ++i) {
+            lists[i] = new ArrayList<>();
+        }
+    }
+
+    public void put(int key, int value) {
+        
+    }
+
+}
